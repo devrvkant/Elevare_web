2bb8b7270142919b6f3e2f52d11321bd450e20ed
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2025-11-09 16:46:49 +0530

    redux toolkit setup on client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { RouterProvider } from "react-router/dom";
+import { Provider } from "react-redux";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 import "./index.css";
 import router from "./routes.jsx";
 import env from "./config/env";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={env.PUBLISHABLE_KEY}>
-      <RouterProvider router={router} />
-    </ClerkProvider>
+    <Provider store={store}>
+      <ClerkProvider publishableKey={env.PUBLISHABLE_KEY}>
+        <RouterProvider router={router} />
+      </ClerkProvider>
+    </Provider>
   </StrictMode>
 );
