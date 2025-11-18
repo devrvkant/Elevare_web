f9e13459f59b0929e5b60a4cad58deed114eca3c
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2025-11-18 13:51:28 +0530

    career api url issue fixed from frontend

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,3 +1,5 @@
+import { careerApi } from "../features/career/careerApi";
+
 const API_URL = import.meta.env.VITE_BACKEND_URL;
 const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
 const FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
@@ -6,6 +8,7 @@ const FIREBASE_STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
 const FIREBASE_MESSAGING_SENDER_ID = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
 const FIREBASE_APP_ID = import.meta.env.VITE_FIREBASE_APP_ID;
 const FIREBASE_MEASUREMENT_ID = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+const CAREER_API_URL = import.meta.env.VITE_CAREER_API_URL;
 
 export const config = {
   apiUrl: API_URL,
@@ -16,4 +19,5 @@ export const config = {
   firebaseMessagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   firebaseAppId: FIREBASE_APP_ID,
   firebaseMeasurementId: FIREBASE_MEASUREMENT_ID,
+  careerApiUrl: CAREER_API_URL,
 }
--- client/src/features/career/careerApi.js
+++ client/src/features/career/careerApi.js
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { config } from "../../config/env.js";
 
 export const careerApi = createApi({
   reducerPath: "careerApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5500/api/career" }),
+  baseQuery: fetchBaseQuery({ baseUrl: config.careerApiUrl }),
   endpoints: (builder) => ({
     predictCareer: builder.mutation({
       query: (body) => ({
